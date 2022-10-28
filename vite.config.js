8321881b6249cd883113a2c6dcb16d60d95450a6
realgautamjakhar <realgautamjakhar@gmail.com>
2022-10-28 21:10:43 +0530

    ....

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
-
+import path from 'path'
 export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
@@ -9,7 +9,17 @@ export default defineConfig(({ command, mode }) => {
     define: {
       __APP_ENV__: env.APP_ENV
     },
-    root: "src"
+    root: path.resolve(__dirname, 'src'),
+    build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main:path.resolve(__dirname, 'src/index.html'),
+        login: path.resolve(__dirname, 'src/login/login.html'),
+        dashboard: path.resolve(__dirname, 'src/dashboard/dashboard.html')
+      }
+    }
+  }
   }
 
 })
